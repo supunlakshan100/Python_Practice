c6aaf41b0f7a20312d4d827ac85ef9b8ad9c3cb6
supunlakshan100 <lakshans793@gmail.com>
2022-12-27 23:29:02 +0530

    git@github.com:supunlakshan100/Python_Practice.git

---
+++ practice01.py
@@ -0,0 +1,32 @@
+print('supun')
+print('ss')
+print('123')
+print(True)
+print(type(True))
+print('supun \n lakshan') #escape character
+print('supun said "good morning !" ')
+name = 'supun'
+print(name)
+
+#math operation
+a=2
+b=3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#concatination
+
+a="Hello"
+b=" World"
+
+d=a+b
+print(d)
+
+a>b
+print(a==b)
