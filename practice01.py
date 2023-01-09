598e249a77495824cc1a48d970617b165b72626c
supunlakshan100 <lakshans793@gmail.com>
2023-01-10 00:09:00 +0530

    edited 09

--- practice01.py
+++ practice01.py
@@ -133,4 +133,27 @@ print(y)
 print(1 in x)
 
 
+#tuple
+
+x=(1,2,3,4,5,6,7,'supun',8,9,10)
+print(type(x))
+supun=x[7]
+print(supun)
+print(x[0:3])
+
+print(x.count(1))
+print(type(x),x)
+
+x=d
+print(d)
+
+#slicing
+
+x=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+y=x[-1]
+print(y)
+print(x)
+
+c=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+print(len(y)) #lenth
 
