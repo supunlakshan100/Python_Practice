4a1f1c2a01bb37b9220e45362d0177adabe57f99
supunlakshan100 <lakshans793@gmail.com>
2023-01-25 00:43:49 +0530

    edited

--- practice04.py
+++ practice04.py
@@ -1,19 +1,171 @@
-class MyFristClass:
-     name='unknown' # attributes,variables
-     bread='unknown'
+'''CLASS 01'''
+
+# class MyFristClass:
+#      name='unknown' # attributes,variables
+#      bread='unknown'
      
-     def bark(self,message): # method ,function,behaviour,action 
-         '''self pass the object of the class to the function ''' #docstring
-         print('inside')
-        #  print(type(x),x) 
-         print('woof!')
+#      def bark(self,message): # method ,function,behaviour,action 
+#          '''self pass the object of the class to the function ''' #docstring
+#          print('inside')
+#         #  print(type(x),x) 
+#          print('woof!')
 
-dog1 = MyFristClass() # create an object ,instance of the class
+# dog1 = MyFristClass() # create an object ,instance of the class
 
-dog1.name = 'Tommy' # add attributes to the object, name is the parameter of the attribute
-# dog1.age = 5
-# dog1.breed = 'Labrador'
-print(dog1.name)
-print('outside',dog1)
+# dog1.name = 'Tommy' # add attributes to the object, name is the parameter of the attribute
+# # dog1.age = 5
+# # dog1.breed = 'Labrador'
+# print(dog1.name)
+# print('outside',dog1)
 
-dog1.bark('hello')
+# dog1.bark('hello')
+
+# MyFristClass.bark(dog1,'hello') 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''CLASS 02'''
+
+# class Dogd: #blueprint of the entity
+#     name='unknown' #attributes,properties
+#     bread='unknown'
+    
+#     def bark(self,message): #method,behaviour,action,functions
+#         print('inside',self)
+#         print('woof woof!',message)
+    
+
+# dog1=Dogd() #object
+
+# print('outside',dog1)
+# dog1.bark('hello') #method call,(parameter)
+
+
+
+
+# dog2=Dogd()#object
+# dog2.bark('hello') #method call,(parameter)
+
+
+
+# Dogd.bark(dog1,'hello') #method call,(parameter)
+
+
+
+
+
+
+
+
+# '''CLASS 03'''
+
+# class Srilankan:
+#     name='' #attributes
+#     age=''
+#     country=''
+#     job=''
+    
+
+#     ''' pass   #empty class,key word'''
+    
+       
+        
+#     def travel_anuradhapura(self,message):#methord(in class function), self is the argument,parameter
+        
+#         print('traveling')
+
+# traveler1=Srilankan() #object1
+
+# traveler1.name='John' #attribute
+# traveler1.age=36
+# traveler1.country='Sri Lanka'
+# traveler1.job='teacher'
+
+# traveler2=Srilankan() #object2
+
+# traveler2.name='Jane' #attribute
+# traveler2.age=32
+# traveler2.country='america'
+# traveler2.job='doctor'
+
+# traveler3=Srilankan() #object3
+
+# traveler3.name='Jill' #attribute
+# traveler3.age=40
+
+
+
+# traveler1.travel_anuradhapura(' pasikuda') #method call
+# traveler2.travel_anuradhapura(' kandy')
+# traveler1.travel_anuradhapura(' anuradhapura')
+
+
+
+
+
+
+
+
+'''CLASS 04'''
+
+class Srilankan:
+    name='' #attributes
+    age=''
+    country=''
+    job=''
+    
+
+    ''' pass   #empty class,key word'''
+    
+       
+        
+    def travel_anuradhapura(self,message):#methord(in class function), self is the argument,parameter
+        
+        print('traveling')
+
+traveler1=Srilankan() #object1
+
+traveler1.name='John' #attribute
+traveler1.age=36
+traveler1.country='Sri Lanka'
+traveler1.job='teacher'
+
+traveler2=Srilankan() #object2
+
+traveler2.name='Jane' #attribute
+traveler2.age=32
+traveler2.country='america'
+traveler2.job='doctor'
+
+traveler3=Srilankan() #object3
+
+traveler3.name='Jill' #attribute
+traveler3.age=40
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
